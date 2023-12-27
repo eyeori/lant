c1df053055e6e6df780d15b55c90f15077d73ca8
傅皆绮 <hzfujieqi@corp.netease.com>
2023-12-27 20:11:01 +0800

    [FIX] fix command line help doc

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ mod utils;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "lant", about = "LAN Transfer")]
 enum Command {
+    #[structopt(about = "Running a lant server")]
     Server {
         #[structopt(
             short,
             long,
-            about = "As a server, provide a port for incoming connections"
+            long_help = "As a server, provide a port for incoming connections"
         )]
         listen_on: u16,
 
@@ -24,41 +25,43 @@ enum Command {
             short,
             long,
             parse(from_os_str),
-            about = "As a server, provide a root path for the client to operate"
+            long_help = "As a server, provide a root path for the client to operate"
         )]
         root_path: PathBuf,
     },
+    #[structopt(about = "Execute a lant client command")]
     Client {
         #[structopt(
             short,
             long,
-            about = "As a client, provide the 'ip:port' of the server to which you want to connect"
+            long_help = "As a client, provide the 'ip:port' of the server to which you want to connect"
         )]
         connect_to: String,
 
-        #[structopt(subcommand, about = "As a client, provide the operator")]
+        #[structopt(subcommand)]
         sub_command: ClientSubCommand,
     },
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(about = "Client sub command")]
 enum ClientSubCommand {
+    #[structopt(about = "List the contents of the specified path")]
     Ls {
         #[structopt(
             short,
             long,
             parse(from_os_str),
-            about = "Gets the contents of the given path"
+            long_help = "Gets the contents of the given path"
         )]
         path_on_remote: PathBuf,
     },
+    #[structopt(about = "Put a file to the specified path")]
     Put {
         #[structopt(
             short,
             long,
             parse(from_os_str),
-            about = "Local file that need to push"
+            long_help = "Local file that need to push"
         )]
         file_path: PathBuf,
 
@@ -66,16 +69,17 @@ enum ClientSubCommand {
             short,
             long,
             parse(from_os_str),
-            about = "Remote dir where the file push to"
+            long_help = "Remote dir where the file push to"
         )]
         remote_dir: PathBuf,
     },
+    #[structopt(about = "Get a file from the specified path")]
     Get {
         #[structopt(
             short,
             long,
             parse(from_os_str),
-            about = "Remote file that need to get"
+            long_help = "Remote file that need to get"
         )]
         file_path: PathBuf,
 
@@ -83,7 +87,7 @@ enum ClientSubCommand {
             short,
             long,
             parse(from_os_str),
-            about = "Local dir where the file save on"
+            long_help = "Local dir where the file save on"
         )]
         local_dir: PathBuf,
     },
